68e027591e5cb5422014cda0a2df3216d1d9aac5
Nadezhda Zizevskaya <7_nadezhda@mail.ru>
2021-05-14 22:18:57 +0400

    first commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int ticketPriceInKopecks = 13500_00; //в копейках
+        int bonusInKopecks = 20_00; //в копейках
+        int totalMills = ticketPriceInKopecks / bonusInKopecks; //бонусные мили
+
+        System.out.println("При покупке билета на сумму " + ticketPriceInKopecks / 100 + " руб. Вам начислено " + totalMills + " бонусных миль");
+
+    }
+}
