922d99379c58467cf7d798d99b16fda9f1b05215
Nadezhda <7_nadezhda@mail.ru>
2023-04-02 20:26:12 +0400

    update repo

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
 
         int ticketPriceInKopecks = 13500_00; //в копейках
         int bonusInKopecks = 20_00; //в копейках
-        int totalMills = ticketPriceInKopecks / bonusInKopecks; //бонусные мили
+        int totalMills = ticketPriceInKopecks / bonusInKopecks;  //бонусные мили
 
         System.out.println("При покупке билета на сумму " + ticketPriceInKopecks / 100 + " руб. Вам начислено " + totalMills + " бонусных миль");
 
